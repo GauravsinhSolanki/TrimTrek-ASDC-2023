4ec8081798e3032507f18376c415ed00bc356215
ranpariya <rk371915@dal.ca>
2023-06-17 12:42:04 -0300

    Add maven version

---
+++ src/test/java/org/example/testMain.java
@@ -0,0 +1,11 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class testMain {
+    @Test
+    public void testMain() {
+        String test = "test";
+        Assertions.assertEquals(test , "test");
+    }
+}
