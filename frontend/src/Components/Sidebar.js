1ffb49fb752a71b6e2c82790b7984ce45afbc74b
jnrana <jy834177@dal.ca>
2023-07-04 00:41:06 -0300

    Adding Navbar and Sidebar

---
+++ frontend/src/Components/Sidebar.js
@@ -0,0 +1,27 @@
+//import React from 'react'
+
+export const Sidebar = [
+    {
+        title: "Update Profile",
+        path: '/UpdateProfile',
+        className: 'side-text'
+    },
+
+    {
+        title: "History",
+        path: '/history',
+        className: 'side-text'
+    },
+
+    {
+        title: "Become a member",
+        path: '/Member',
+        className: 'side-text'
+    },
+
+    {
+        title: "Register as a barber",
+        path: '/BarberRegister',
+        className: 'side-text'
+    },
+]
