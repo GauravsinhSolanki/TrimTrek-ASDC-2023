093626377b8363aacd7bbb8c5307fff9f076557a
GauravsinhSolanki <solankigbs11@gmail.com>
2023-07-18 21:45:16 -0300

    Updated Navbar with Styling

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../Components/navbar.css";
+import { Link } from 'react-router-dom'
+
+import Sidebarmenu from '../Components/Sidebarmenu';
+
+const Navbar = () => {
+ 
+  return (
+    <header className = 'navbar'>
+      <nav className='navbar_navigation'>
+        <div></div>
+        <Sidebarmenu />
+        <div className='helper'></div>
+        <div className='nav_items'>
+          <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/'>Book Now</Link></li>
+                <li><Link to='/'>Gallery</Link></li>
+                <li><Link to='/offersPage'>Offer & Deals</Link></li>
+            </ul>
+        </div>
+      </nav>
+    </header>
+    
+  )
+}
+
+export default Navbar;
