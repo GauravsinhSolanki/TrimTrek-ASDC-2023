2931c0725a8c43d8ce056788437354c956566b81
GauravsinhSolanki <solankigbs11@gmail.com>
2023-07-18 22:35:37 -0300

    Barber Register Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -8,33 +7,17 @@ import BarberRegister from "./Components/BarberRegister";
 import Navbar from "./Components/Navbar";
 import SignupPage from "./Components/Authentication/Signup";
 import OffersPage from "./Components/offerPage";
-=======
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Components/Authentication/Login';
-import Home from './Components/Home';
-import BarberRegister from './Components/BarberRegister';
-import BarberRegisterNext from './Components/BarberRegisterNext';
->>>>>>> cef8aede988707560fbb8ab652aa155369f9bee5
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-<<<<<<< HEAD
           <Route exact path="/" element={<Login />} />
           <Route exact path="/home" element={<Home />} />
           <Route exact path="/signup" element={<SignupPage />} />
           <Route exact path="/BarberRegister" element={<BarberRegister />} />
           <Route exact path="/offersPage" element={<OffersPage />} />
-=======
-          <Route exact path="/" element={<Login/>} />
-          <Route exact path='/home' element={<Home/>}/>  
-          <Route exact path='/BarberRegister' element={<BarberRegister/>}/>  
-          <Route exact path='/BarberRegisterNext' element={<BarberRegisterNext/>}/>     
->>>>>>> cef8aede988707560fbb8ab652aa155369f9bee5
         </Routes>
       </Router>
     </div>
--- frontend/src/Components/BarberRegister.js
+++ frontend/src/Components/BarberRegister.js
@@ -1,83 +1,9 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './BarberRegister.css';
-import Navbar from "./Navbar";
-const BarberRegister = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [birthdate, setBirthdate] = useState('');
-  const [address, setAddress] = useState('');
-  const navigate = useNavigate();
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
+import React from 'react'
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleBirthdateChange = (e) => {
-    setBirthdate(e.target.value);
-  };
-
-  const handleAddressChange = (e) => {
-    setAddress(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
-    // This is just a sample, you can replace it with your own logic
-    console.log('Form submitted:', { name, email, password, birthdate, address });
-    navigate('/BarberRegisterNext');
-  };
-
-  const handleBackToHomepage = () => {
-    // Redirect to the homepage
-    navigate('/home');
-  };
-
+export default function BarberRegister() {
   return (
     <div>
-      <div>
-        <Navbar />
-      </div>
-      <div className="signup-container">
-        <h1>Register as Barber</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-field">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" value={name} onChange={handleNameChange} required />
-          </div>
-          <div className="form-field">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" value={email} onChange={handleEmailChange} required />
-          </div>
-          <div className="form-field">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" value={password} onChange={handlePasswordChange} required />
-          </div>
-          <div className="form-field">
-            <label htmlFor="birthdate">Birthdate:</label>
-            <input type="date" id="birthdate" value={birthdate} onChange={handleBirthdateChange} required />
-          </div>
-          <div className="form-field">
-            <label htmlFor="address">Address:</label>
-            <input type="text" id="address" value={address} onChange={handleAddressChange} required />
-          </div>
-          <div className="form-buttons">
-            <button type="button" onClick={handleBackToHomepage}>Back to Homepage</button>
-            <button type="submit">Next Page</button>
-          </div>
-        </form>
-      </div>
+      <p>Hello</p>
     </div>
-  );
-};
-
-export default BarberRegister;
+  )
+}
