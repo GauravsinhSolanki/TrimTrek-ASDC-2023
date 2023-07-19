977b5ff5b4a38194da128c4ed3025f4411818193
GauravsinhSolanki <solankigbs11@gmail.com>
2023-07-18 22:06:21 -0300

    Routing Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Components/Authentication/Login';
-
+import Login from "./Components/Authentication/Login";
+import Home from "./Components/Home";
+import BarberRegister from "./Components/BarberRegister";
+import Navbar from "./Components/Navbar";
+import SignupPage from "./Components/Authentication/Signup";
+import OffersPage from "./Components/offerPage";
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
-          <Route exact path="/" element={<Login/>}>           
-          </Route>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/signup" element={<SignupPage />} />
+          <Route exact path="/BarberRegister" element={<BarberRegister />} />
+          <Route exact path="/offersPage" element={<OffersPage />} />
         </Routes>
       </Router>
     </div>
