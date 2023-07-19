91acfd273f58e58c290a85dfec775f5b4f8332c0
GauravsinhSolanki <solankigbs11@gmail.com>
2023-07-18 22:28:13 -0300

    Navbar Updated

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,15 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import "../Components/navbar.css";
 import { Link } from 'react-router-dom'
 
 import Sidebarmenu from '../Components/Sidebarmenu';
-=======
-import './Navbar.css';
-import { Link } from 'react-router-dom'
-
-import Sidebarmenu from './Sidebarmenu';
->>>>>>> cef8aede988707560fbb8ab652aa155369f9bee5
 
 const Navbar = () => {
  
@@ -18,21 +11,13 @@ const Navbar = () => {
       <nav className='navbar_navigation'>
         <div></div>
         <Sidebarmenu />
-<<<<<<< HEAD
-=======
-
->>>>>>> cef8aede988707560fbb8ab652aa155369f9bee5
         <div className='helper'></div>
         <div className='nav_items'>
           <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/'>Book Now</Link></li>
                 <li><Link to='/'>Gallery</Link></li>
-<<<<<<< HEAD
                 <li><Link to='/offersPage'>Offer & Deals</Link></li>
-=======
-                <li><Link to='/'>Offer & Deals</Link></li>
->>>>>>> cef8aede988707560fbb8ab652aa155369f9bee5
             </ul>
         </div>
       </nav>
