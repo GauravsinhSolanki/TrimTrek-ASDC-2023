e1e73650649f21d7836b7e3432a0acfdb071ae4d
NEEL PIYUSHKUMAR PATEL <nl914739@dal.ca>
2023-07-22 10:11:05 +0530

    Refresh Code

--- backend/src/main/java/org/example/BarberRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BarberRepository extends JpaRepository<Barber, Long> {
-}
