3ee507504b8e255ca924c7578083baeb844135c0
NEEL PIYUSHKUMAR PATEL <nl914739@dal.ca>
2023-07-24 03:32:52 +0530

    User Authentication - user

---
+++ backend/Projectdemo1/src/main/java/com/ProjectTrial1/Projectdemo1/account/user/BarberDto.java
@@ -0,0 +1,13 @@
+package com.ProjectTrial1.Projectdemo1.account.user;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BarberDto {
+    private String emailId;
+    private String speciality;
+}
