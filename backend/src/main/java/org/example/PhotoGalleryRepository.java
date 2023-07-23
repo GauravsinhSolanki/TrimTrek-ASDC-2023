b3a47f19fb5581108aa2072deca70702848b69b2
jnrana <jy834177@dal.ca>
2023-07-23 18:37:51 -0300

    .

--- backend/src/main/java/org/example/PhotoGalleryRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhotoGalleryRepository extends JpaRepository<PhotoGallery, Long> {
-}
