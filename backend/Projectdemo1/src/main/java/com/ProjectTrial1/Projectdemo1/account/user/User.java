fb937f63420d99c286bbe106ef819b494cf64493
NEEL PIYUSHKUMAR PATEL <nl914739@dal.ca>
2023-07-24 02:53:52 +0530

    User Authentication-Role, backend

---
+++ backend/Projectdemo1/src/main/java/com/ProjectTrial1/Projectdemo1/account/user/User.java
@@ -0,0 +1,62 @@
+package com.ProjectTrial1.Projectdemo1.account.user;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "user_id", unique = true)
+	private String userId;
+
+	@Column(name = "user_name")
+	private String userName;
+
+	@Column(name = "user_password")
+	private String userPassWord;
+
+	@Column(name = "gender")
+	private String gender;
+
+	@Column(name = "dob", columnDefinition = "Date")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dob;
+
+	@Column(name = "phone_no")
+	private String phoneNo;
+
+	@Column(name = "alt_phone_no")
+	private String altPhoneNo;
+
+	@Column(name = "email_id")
+	private String emailId;
+
+	@Column(name = "speciality")
+	private String speciality;
+
+	@Column(name = "is_active")
+	private boolean isActive = true;
+
+	@Column(name = "created_by")
+	private String createdBy;
+	@Column(name = "created_on")
+	private LocalDateTime createdOn;
+	@Column(name = "updated_by")
+	private String updatedBy;
+	@Column(name = "updated_on")
+	private LocalDateTime updatedOn;
+
+}
---
+++ backend/Projectdemo1/src/main/java/com/ProjectTrial1/Projectdemo1/account/user/UserServices.java
@@ -0,0 +1,210 @@
+package com.ProjectTrial1.Projectdemo1.account.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class UserServices {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserServices.class);
+
+    @Autowired
+    UserRepository userRepo;
+
+//    @Autowired
+//    GardenServices gardenServices;
+//
+//    @Autowired
+//    AddressServices addressServices;
+//
+//    @Autowired
+//    GardeningTaskServices gardeningTaskServices;
+
+    public User createUser(UserDto userDto) {
+        LOG.debug("createUser, userDto: " + userDto);
+        User user = convertUserDtotoEntity(userDto);
+        User dbuser = userRepo.save(user);
+        LOG.debug("createUser, dbuser: " + dbuser);
+        return dbuser;
+    }
+
+    private User convertUserDtotoEntity(UserDto userDto) {
+        LOG.debug("convertUserDtotoEntity, userDto: " + userDto);
+        User user = new User();
+        String generatedString = generateUniqueUserId();
+        user.setUserId(generatedString);
+        user.setUserName(userDto.getUserName());
+        user.setUserPassWord(userDto.getUserPassWord());
+        user.setGender(userDto.getGender());
+        user.setDob(userDto.getDob());
+        user.setPhoneNo(userDto.getPhoneNo());
+        user.setAltPhoneNo(userDto.getAltPhoneNo());
+        user.setEmailId(userDto.getEmailId());
+        user.setCreatedOn(LocalDateTime.now());
+        LOG.debug("convertUserDtotoEntity, user: " + user);
+        return user;
+    }
+    private String generateUniqueUserId() {
+        String generatedUserId = RandomStringUtils.random(11, true, true);
+        return generatedUserId;
+    }
+
+
+    public List<UserDto> getAllUsers() {
+        LOG.debug("getAllUsers,  ");
+        List<UserDto> dtos = new ArrayList<>();
+
+        List<User> users = userRepo.findAll();
+
+        for (User user : users) {
+            UserDto dto = convertUserEntitytoDto(user);
+            dtos.add(dto);
+        }
+        LOG.debug("getAllUsers, dtos: " + dtos);
+        return dtos;
+    }
+
+    public UserDto convertUserEntitytoDto(User user) {
+        LOG.debug("convertUserEntitytoDto, user: " + user);
+        UserDto userDto = new UserDto();
+
+        userDto.setUserName(user.getUserName());
+        userDto.setUserPassWord(user.getUserPassWord());
+        userDto.setGender(user.getGender());
+        userDto.setDob(user.getDob());
+        userDto.setPhoneNo(user.getPhoneNo());
+        userDto.setAltPhoneNo(user.getAltPhoneNo());
+        userDto.setEmailId(user.getEmailId());
+        LOG.debug("convertUserEntitytoDto, userDto: " + userDto);
+        return userDto;
+    }
+
+
+
+    public UserDto getUserByUserId(String userId) {
+        LOG.debug("getUserByUserId, userId: " + userId);
+        User User = userRepo.findByUserId(userId);
+
+        UserDto dto = convertUserEntitytoDto(User);
+        LOG.debug("getUserByUserId, dto: " + dto);
+        return dto;
+    }
+
+
+
+//	public User blockUser(String userId) {
+//		LOG.debug("blockUser, userId: " + userId);
+//		User user = userRepo.findByEmailId(userId);
+//
+//		user.setActive(false);
+//		userRepo.save(user);
+//
+////		 List<UserRole> userRoles = userRoleRepo.findByUserId(user.getId()); TODO
+////		 for(UserRole userRole : userRoles ) {
+////			 userRole.setActive(false);
+////			 userRoleRepo.save(userRole);
+////		 }
+//		gardenServices.inActivategardensByUserId(userId);
+//		addressServices.inActivateAddressByUserId(userId);
+//		LOG.debug("blockUser, user: " + user);
+//		return user;
+//	}
+
+	public UserDto getUserByEmailId(String emailId) {
+		LOG.debug("getUserByEmailId, emailId: " + emailId);
+		User user = userRepo.findByEmailId(emailId);
+		UserDto dto = convertUserEntitytoDto(user);
+		LOG.debug("getUserByEmailId, dto: " + dto);
+		return dto;
+	}
+
+	public String getUserIdByEmailId(String emailId) {
+		LOG.debug("getUserIdByEmailId, emailId: " + emailId);
+		User user = userRepo.findByEmailId(emailId);
+		String userId = user.getEmailId();
+		LOG.debug("getUserIdByEmailId, userId: " + userId);
+		return userId;
+	}
+
+	public String getEmailIdByUserId(String userId) {
+		LOG.debug("getEmailIdByUserId, userId: " + userId);
+		User user = userRepo.findByUserId(userId);
+		String emailId = user.getEmailId();
+		LOG.debug("getEmailIdByUserId, emailId: " + emailId);
+		return emailId;
+	}
+
+    public String getPasswordByEmailId(String emailId){
+        LOG.debug("getPasswordByEmailId, emailId: " + emailId);
+        User user = userRepo.findByEmailId(emailId);
+        String passWord = user.getUserPassWord();
+        LOG.debug("getPasswordByEmailId, passWord: " + passWord);
+        return passWord;
+    }
+
+    public ResponseEntity<String> signIn(String emailId, String userPassWord) {
+        LOG.debug("signIn, emailId: " + emailId);
+        LOG.debug("signIn, userPassWord: " + userPassWord);
+       User user = userRepo.findByEmailId(emailId);
+
+        if (user == null) {
+            return ResponseEntity.badRequest().body("Invalid username.");
+        }
+
+       String passWord = getPasswordByEmailId(emailId);
+
+        if (!(passWord.equals(userPassWord) )){
+            return ResponseEntity.badRequest().body("Invalid password.");
+        }
+
+        return ResponseEntity.ok("Sign-in successful!");
+    }
+
+
+
+    public User createBarberProfile(BarberDto barberDto) {
+        LOG.debug("barberProfile, barberDto: " + barberDto);
+
+        User user =  userRepo.findByEmailId(barberDto.getEmailId());
+        System.out.println(user);
+        user.setSpeciality(barberDto.getSpeciality());
+        System.out.println(user);
+        User dbuser = userRepo.update(user);
+        LOG.debug("createUser, dbuser: " + dbuser);
+        return dbuser;
+    }
+
+
+
+    public BarberProfileDto getBarberProfileByEmailId(String emailId) {
+        LOG.debug("getBarberProfileByEmailId, emailId: " + emailId);
+        User user = userRepo.findByEmailId(emailId);
+        BarberProfileDto dto = convertUserEntitytoBarberProfileDto(user);
+        LOG.debug("getBarberProfileByEmailId, dto: " + dto);
+        return dto;
+    }
+
+    public BarberProfileDto convertUserEntitytoBarberProfileDto(User user) {
+        LOG.debug("convertUserEntitytoBarberProfileDto, user: " + user);
+        BarberProfileDto barberProfileDto = new BarberProfileDto();
+
+        barberProfileDto.setUserName(user.getUserName());
+        barberProfileDto.setGender(user.getGender());
+        barberProfileDto.setDob(user.getDob());
+        barberProfileDto.setPhoneNo(user.getPhoneNo());
+        barberProfileDto.setAltPhoneNo(user.getAltPhoneNo());
+        barberProfileDto.setEmailId(user.getEmailId());
+        barberProfileDto.setSpeciality(user.getSpeciality());
+        LOG.debug("convertUserEntitytoBarberProfileDto, barberProfileDto: " + barberProfileDto);
+        return barberProfileDto;
+    }
+}
