722354a70f41d3ad010c3bfcb0385a3a161721be
jnrana <jy834177@dal.ca>
2023-07-31 16:04:00 -0300

    BarberInventory Update

--- backend/Projectdemo1/src/main/java/com/ProjectTrial1/Projectdemo1/barberinventory/BarberInventoryService.java
+++ backend/Projectdemo1/src/main/java/com/ProjectTrial1/Projectdemo1/barberinventory/BarberInventoryService.java
@@ -1,37 +1,48 @@
+// Package declaration
 package com.ProjectTrial1.Projectdemo1.barberinventory;
 
+// Importing necessary libraries and annotations
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+// Annotating the class as a Spring Service, indicating it's a service component
 @Service
 public class BarberInventoryService {
 
-
+    // Autowiring the BarberInventoryRepository for data access
     @Autowired
     BarberInventoryRepository barberInventoryRepository;
 
+    // Method to save a BarberInventory item in the inventory
     public BarberInventory saveBarberInventory(BarberInventory barberInventory) {
-        String generatedString = generateUniqueUserId();
-
+        // Generate a unique product ID and set it in the BarberInventory object
+        String generatedString = generateUniqueProductId();
         barberInventory.setProductId(generatedString);
+
+        // Save the BarberInventory object to the database using the repository
         return barberInventoryRepository.save(barberInventory);
     }
 
-    private String generateUniqueUserId() {
+    // Helper method to generate a unique product ID
+    private String generateUniqueProductId() {
         String generatedProductId = RandomStringUtils.random(11, true, true);
         return generatedProductId;
     }
 
+    // Method to get all BarberInventory items from the inventory
     public List<BarberInventory> getAllBarberInventories() {
         return barberInventoryRepository.findAll();
     }
 
-    public BarberInventory findByBarberIdAndProductName(String emailId , String productName) {
-        return barberInventoryRepository.findByBarberIdAndProductName(emailId,productName);
+    // Method to find a BarberInventory item by barberId and productName
+    public BarberInventory findByBarberIdAndProductName(String emailId, String productName) {
+        return barberInventoryRepository.findByBarberIdAndProductName(emailId, productName);
     }
 
+    // Method to reduce the quantity of a product in the inventory
     public BarberInventory reduceProductQuantity(String id, int quantityToReduce) {
         BarberInventory barberInventory = barberInventoryRepository.findByEmailId(id);
 
@@ -40,47 +51,40 @@ public class BarberInventoryService {
             if (currentQuantity >= quantityToReduce) {
                 int newQuantity = currentQuantity - quantityToReduce;
                 barberInventory.setQuantity(newQuantity);
+                // Update the reduced quantity in the database using the repository
                 return barberInventoryRepository.updateQuantity(id, newQuantity);
             } else {
-
                 throw new IllegalArgumentException("Insufficient quantity for product with ID: " + id);
             }
         } else {
-
             throw new IllegalArgumentException("Product with ID: " + id + " not found in the inventory.");
         }
     }
 
+    // Method to perform deduction of products for a specific service
     public BarberInventory performDeductionOperation(String barberId, String serviceName) {
-        System.out.println("Perform deduction");
         BarberInventory barberInventory = null;
         String[] products = new String[0];
-        System.out.println("0"+ products);
+
+        // Determine the list of products to be deducted based on the service name
         if (serviceName.equals("Haircut")) {
-          products= new String[]{"Shampoo", "Scissor"};
-            System.out.println("1" + products);
-        } else if (serviceName.equals("Beard") ) {
-         products = new String[]{"Scissor", "ShavingCream"};
-            System.out.println("2" + products);
-        } else if (serviceName.equals("MustacheGrooming") ) {
-         products = new String[]{"Comb", "BeardOil"};
-        } else if (serviceName.equals("facials") ) {
+            products = new String[]{"Shampoo", "Scissor"};
+        } else if (serviceName.equals("Beard")) {
+            products = new String[]{"Scissor", "ShavingCream"};
+        } else if (serviceName.equals("MustacheGrooming")) {
+            products = new String[]{"Comb", "BeardOil"};
+        } else if (serviceName.equals("Facials")) {
             products = new String[]{"FaceCleanser", "FacialMask"};
-            System.out.println("3" + products);
-        } else if (serviceName.equals("Hair Treatment") ) {
+        } else if (serviceName.equals("Hair Treatment")) {
             products = new String[]{"HairConditioners", "HairSpray"};
-            System.out.println("4" + products);
         }
 
+        // Deduct the quantity of each product from the inventory
         for (String productNameIndividual : products) {
-            System.out.println("for loop");
             barberInventory = barberInventoryRepository.findByBarberIdAndProductName(barberId, productNameIndividual);
-            System.out.println(barberInventory);
             int updatedQuantity = barberInventory.getQuantity() - 1;
-            System.out.println(updatedQuantity);
             barberInventory.setQuantity(updatedQuantity);
-          barberInventoryRepository.updateQuantityNew(productNameIndividual, updatedQuantity,barberId);
-
+            barberInventoryRepository.updateQuantityNew(productNameIndividual, updatedQuantity, barberId);
         }
 
         return barberInventory;
